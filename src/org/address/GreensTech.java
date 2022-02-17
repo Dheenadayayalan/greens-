648ec243a8f22c0895e5452f926f2f5938eb0c2f
Dheenadayalan <dheenadayalank95@gmail.com>
2022-02-17 12:36:54 +0530

    greens adress

---
+++ src/org/address/GreensTech.java
@@ -0,0 +1,33 @@
+package org.address;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("greensOmr");
+
+	}
+	private void greensAdayar() {
+		System.out.println("greensAdayar");
+
+	}
+	private void greensVelacherry() {
+		System.out.println("greensVelacherry");
+
+	}
+	private void greensTambaram() {
+		System.out.println("greensTambaram");
+
+	}
+	private void greensAnnaNagar() {
+	    System.out.println("greensAnnaNagar");
+
+	}
+	public static void main(String[] args) {
+		GreensTech b = new GreensTech();
+		b.greensOmr();
+		b.greensAdayar();
+		b.greensVelacherry();
+		b.greensTambaram();
+		b.greensAnnaNagar();
+	}
+
+}
